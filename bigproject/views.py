b4857ef919b09791e4e195e587a1643bdc270f61
YIFUNLIN <drose01rrr@gmail.com>
2023-07-31 02:25:55 +0800

    first

--- bigproject/views.py
+++ bigproject/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+"""
 def home(request):
     user_input = request.GET.get('input_text', '')  # 獲取用戶的輸入
     return render(request, 'index.html', {'output_text': user_input})  # 將用戶的輸入傳遞到模板中
+"""
+
+def home(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('input_text', '')  # 獲取用戶的輸入
+        return render(request, 'index.html', {'output_text': user_input})  # 將用戶的輸入傳遞到模板中
+    else:
+        return render(request, 'index.html')
+
